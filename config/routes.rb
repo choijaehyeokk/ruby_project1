8bafa00122a9101802abea286b18d37b2da3a0da
최재혁 <enlqn1010@naver.com>
2020-07-11 14:56:24 +0000

    version_5

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	get '/' => 'home#index'
 	#'/'로 접속을 하면 home controller의 index action으로 작업을 하도록 만들어라.
-	get '/login' => 'login#login'
-	
+	get '/login' => 'home#login'
+	get '/signin' => 'home#signin'
 end
