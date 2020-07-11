7bdedaa42ce0011483eeb634cc9cf62222ebe2d6
최재혁 <enlqn1010@naver.com>
2020-07-11 11:25:48 +0000

    version_4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 	
 	def index #여기서 만든 index 메서드를 views에 home dir에 index파일을 만들면 이것과 연결이 됨. + route에도 연결을 시켜야함.
-		
+		@title = "항개발"
+		@login ="로그인"
+		@signin= "회원가입"
 	end
 end
