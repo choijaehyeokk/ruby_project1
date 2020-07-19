c26f7099f20b549413e8795287f019177da1ea8e
LeeJunhee <725lsw@hanmail.net>
2020-07-20 01:19:59 +0900

    main page design ver7.4.1 - delete db

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -3,12 +3,5 @@ class SigninController < ApplicationController
 		
 	end
 	def create
-		user = User.new
-		user.first_name =params[:first_name]
-		user.last_name =params[:last_name]
-		user.email = params[:email]
-		user.save
-		
-		redirect_to :root
 	end
 end
