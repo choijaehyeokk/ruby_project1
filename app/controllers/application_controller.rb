3552ace9ef0e8bfe775c19f81a03c7af7bbaef3a
LeeJunhee <725lsw@hanmail.net>
2020-07-21 03:21:57 +0900

    main page design ver7.5 - search bar&contents link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
     def initialize()
         super()
         @APP_TITLE = "항개발"
+        languages = "C C++ Java Matlab Python"
+        @lang_name = languages.split
+        @lang_uri = "C C%2B%2B Java Matlab Python".split
+        @lang_extension = "c cpp java mat py".split
     end
 end
--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,5 +1,8 @@
+require 'uri'
+
 class ContentsController < ApplicationController
     def contents
-        @APP_TITLE += "-CONTENTS"
+        @APP_TITLE += "-CONTENTS("+params[:name]+")"
+        @CONTENTS_NAME = params[:name]
     end
 end
