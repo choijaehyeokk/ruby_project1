b4490c48f6a99ab6572e91f64aa291f5f93888cf
LeeJunhee <725lsw@hanmail.net>
2020-07-23 00:53:51 +0900

    main page design ver7.6 - contents page

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,8 +1,15 @@
 require 'uri'
 
 class ContentsController < ApplicationController
+    def initialize()
+        super()
+        @CONTENTS_NAME = ""
+        @CONTENTS_INDEX
+    end
+
     def contents
-        @APP_TITLE += "-CONTENTS("+params[:name]+")"
-        @CONTENTS_NAME = params[:name]
+        @CONTENTS_INDEX = params[:index].to_i
+        @CONTENTS_NAME = @lang_name[@CONTENTS_INDEX]
+        @APP_TITLE += "-CONTENTS(#{@CONTENTS_NAME})"
     end
 end
