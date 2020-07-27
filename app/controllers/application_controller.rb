994e2422b693ac3541428002bbd5e92d9412a96b
최재혁 <enlqn1010@naver.com>
2020-07-27 09:17:21 +0000

    add_User_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :null_session
     def initialize()
         super()
         @APP_TITLE = "항개발"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  
+	 authenticate :user, lambda { |u| u.admin? } do
+		begin
+		  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+		rescue
+		  redirect_to new_user_session_path
+		end
+	end
+  devise_for :users
 	root to: 'home#index'
 	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	
